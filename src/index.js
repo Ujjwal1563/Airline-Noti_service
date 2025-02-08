c68c1a605c60bdf0230a59bb0fea5435212c5507
Ujjwal Sharma <ujjwal.shar.ma1563@gmail.com>
2025-02-08 11:51:51 +0530

    completed consumption and sending email

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const amqplib = require("amqplib");
 const { EmailService } = require("./services");
 async function connectQueue() {
@@ -21,16 +21,16 @@ async function connectQueue() {
   } catch (error) {}
 }
 
-
-
-const { ServerConfig } = require('./config');
-const apiRoutes = require('./routes');
+const { ServerConfig } = require("./config");
+const apiRoutes = require("./routes");
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', apiRoutes);
+app.use("/api", apiRoutes);
 
-app.listen(ServerConfig.PORT, () => {
-    console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+app.listen(ServerConfig.PORT, async () => {
+  console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+  await connectQueue();
+  console.log("queue is up");
 });
