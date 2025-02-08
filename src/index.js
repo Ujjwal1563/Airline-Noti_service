deb4f0b0715e91c69cfb37b8386c19099f075479
Ujjwal Sharma <ujjwal.shar.ma1563@gmail.com>
2025-02-08 11:49:00 +0530

    completed consumption and sending email

--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
 const express = require('express');
+const amqplib = require("amqplib");
+const { EmailService } = require("./services");
+async function connectQueue() {
+  try {
+    const connection = await amqplib.connect("amqp://localhost");
+    const channel = await connection.createChannel();
+    await channel.assertQueue("noti-queue");
+    channel.consume("noti-queue", async (data) => {
+      console.log(`${Buffer.from(data.content)}`);
+      const object = JSON.parse(`${Buffer.from(data.content)}`);
+      // const object = JSON.parse(Buffer.from(data).toString());
+      await EmailService.sendEmail(
+        "airlinenoti@gmail.com",
+        object.recepientEmail,
+        object.subject,
+        object.text
+      );
+      channel.ack(data);
+    });
+  } catch (error) {}
+}
+
+
 
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
